7428ffa78fe23e3c0cf47f47e55e5298df0e6e91
christopherzito <christopherzito11@gmail.com>
2022-03-09 10:26:18 +0100

    add payment dropin form

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {        
-        // \Braintree_Configuration::environment(env('BT_ENVIRONMENT'));
-        // \Braintree_Configuration::merchantId(env('BT_MERCHANT_ID'));
-        // \Braintree_Configuration::publicKey(env('BT_PUBLIC_KEY'));
-        // \Braintree_Configuration::privateKey(env('BT_PRIVATE_KEY'));
+        //
     }
 }
--- database/seeds/ApartmentSponsorshipSeeder.php
+++ database/seeds/ApartmentSponsorshipSeeder.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-
+use App\Sponsorship;
 class ApartmentSponsorshipSeeder extends Seeder
 {
     /**
@@ -15,13 +15,31 @@ class ApartmentSponsorshipSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('apartment_sponsorship')->insert([
+        $sponsorships = Sponsorship::All();
+        foreach ($sponsorships as $sponsorship) {
+            for ($t=0; $t < 10; $t++) { 
+                //Prendo il momento attuale
+                $dateTimePayment = date('y-m-d h:i:s');
+                $dateTimeStart = date('y-m-d h:i:s');
+                $dateTimeEnd = new DateTime($dateTimeStart);//lo chiamo nel Datetime
+                if($sponsorship -> id == 1){
+                    $dateTimeEnd->modify("+1 day");//lo "sposto" di 24 ore in avanti
+                }else if($sponsorship -> id == 2){
+                    $dateTimeEnd->modify("+3 day");//lo "sposto" 72 ore in avanti
+                }else{
+                    $dateTimeEnd->modify("+6 day");//lo "sposto" 144 ore in avanti
+                } 
 
-            'payment_dateTime' => '2022-02-25',
-            'start_sponsorship' => '2022-02-25',
-            'end_sponsorship' => '2022-02-25',
-            'apartment_id' => 1,
-            'sponsorship_id' => 2,
-        ]);
+                $apartment = rand(1,20);
+                DB::table('apartment_sponsorship')->insert([
+                    'payment_dateTime' => $dateTimePayment,
+                    'start_sponsorship' => $dateTimeStart,
+                    'end_sponsorship' => $dateTimeEnd,
+                    'apartment_id' => $apartment,
+                    'sponsorship_id' => $sponsorship -> id,
+                ]);
+            }
+        }
+        
     }
 }
